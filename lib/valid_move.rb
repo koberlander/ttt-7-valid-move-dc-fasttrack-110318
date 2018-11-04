dc03cb20c3251b97f48aef2deb09ad10ee33327b
Kristin M Oberlander <koberlander115@gmail.com>
2018-11-04 19:48:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if board[index] <= 9 && position_taken == true
+    return true
+    else  
+    return false 
+  end 
+end
+returns `true` if the move is valid and `false` or `nil` if not. A valid move means that the submitted position is:
 
+1. Present on the game board.
+2. Not already filled with a token.
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken()
+if board[index] == " "
+    return true
